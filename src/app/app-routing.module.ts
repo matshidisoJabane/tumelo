446115694fcd7ec7d550861bc98b16ca92f1c8d7
godfrey <godfreyhuig@gmail.com>
2019-08-05 14:31:13 +0200

    bhyjbn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import { combineLatest } from 'rxjs';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [{path: 'ContactUs', component: ContactUsComponent},
+                        {path: 'AboutUs', component: AboutUsComponent},
+                      {path: 'Home', component: HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
